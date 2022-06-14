c6a104505a91bf75216820f2a648744ff6982220
cater20 <onyangoketta@gmail.com>
2022-06-14 20:46:44 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+document.querySelector("form").addEventListener("submit", (e)=>{
+  e.preventDefault()
+  buildTask(e.target.new_task);
+  format.reset()
+})
+function buildTask(task) {
+  
+  let p=document.createElement("p")
+  let btn=document.createElement("button")
+  btn.addEventListener("click",handledelete)
+  btn.textContent = "x"
+  p.textContent=`${task}`
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector("task").appendChild('p')
+}
+function handledelete(e){
+  e.target.parentNode.remove()
+}
+
 });
